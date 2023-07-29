6add70ac947c7050da26a8fe77836c5afdf2a50c
ManvendraChauhan <manvendrachauhan292001@gmail.com>
2023-07-29 23:33:37 +0530

    YouTube

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key':  'a2e7b556d1msh8a9bcce58b02724p166fbajsn4b84a39ff14a',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
